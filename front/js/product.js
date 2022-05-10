4bd5a9626b274f44889d2400e3f30687ae65f3ce
Michel Vieira <michelvieira.pro@gmail.com>
2022-05-10 17:44:02 +0200

    Finalisation de la page produit

--- front/js/product.js
+++ front/js/product.js
@@ -8,12 +8,19 @@ fetch(url)
     .then((response) =>
         response.json().then((data) => {
             console.log(data);
-            let affichage = '';
+            const { imageUrl, name, price, description, colors, altTxt } = data;
+            document.querySelector('.item__img').innerHTML = 
+                `<img src="${imageUrl}" alt="${altTxt}"></img>`
+            document.querySelector('#title').innerHTML = name;
+            document.querySelector('#price').innerHTML = price;
+            document.querySelector('#description').innerHTML = description;
+            for (let color of colors) {
+                document.querySelector('#colors').innerHTML += 
+                    `<option value="${color}">${color}</option>`
+            }
         }))
     .catch((error) => {
         console.log(error);
-        let affichage = 'Notre site est en cours de maintenance, merci pour votre compréhension.';
-        document.querySelector('#items').innerHTML = affichage;
     }
 
     )
