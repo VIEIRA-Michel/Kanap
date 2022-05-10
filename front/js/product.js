0cf0772f9166d79a0434dc6d42ab7705b5e1eca5
Michel Vieira <michelvieira.pro@gmail.com>
2022-05-10 18:45:45 +0200

    Récupération de l'objet produit, lors du clic sur le bouton 'ajouter au panier'

--- front/js/product.js
+++ front/js/product.js
@@ -9,18 +9,36 @@ fetch(url)
         response.json().then((data) => {
             console.log(data);
             const { imageUrl, name, price, description, colors, altTxt } = data;
-            document.querySelector('.item__img').innerHTML = 
+            document.querySelector('.item__img').innerHTML =
                 `<img src="${imageUrl}" alt="${altTxt}"></img>`
             document.querySelector('#title').innerHTML = name;
             document.querySelector('#price').innerHTML = price;
             document.querySelector('#description').innerHTML = description;
             for (let color of colors) {
-                document.querySelector('#colors').innerHTML += 
+                document.querySelector('#colors').innerHTML +=
                     `<option value="${color}">${color}</option>`
             }
+            document.querySelector('#addToCart').addEventListener("click", function() {
+                let color = document.querySelector('#colors');
+                let quantity = document.querySelector('#quantity');
+                console.log(color.value, quantity.value)
+
+                let produit = {
+                    id: id,
+                    image: imageUrl,
+                    alt: altTxt,
+                    description: description,
+                    name: name,
+                    color: color.value,
+                    price: price,
+                    quantity: quantity.value,
+
+                }
+
+                console.log(produit)
+            });
         }))
     .catch((error) => {
         console.log(error);
-    }
+    });
 
-    )
