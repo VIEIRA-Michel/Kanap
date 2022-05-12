fc9f68c76309bd7481dabffa1f433a137ffcabc9
Michel Vieira <michelvieira.pro@gmail.com>
2022-05-12 19:09:05 +0200

    Ajout du prix total et de la possibilité de pouvoir modifier la quantité d'article depuis le panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,10 +1,9 @@
 let panier = JSON.parse(localStorage.getItem("product"));
-console.log(panier);
-let i = 1
+let prixFinal = 0;
+let bool = false;
 
 for (article of panier) {
-    i++
-    document.querySelector('#cart__items').innerHTML += `
+  document.querySelector('#cart__items').innerHTML += `
     <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
                 <div class="cart__item__img">
                   <img src="${article.image}" alt="${article.alt}">
@@ -17,7 +16,7 @@ for (article of panier) {
                   </div>
                   <div class="cart__item__content__settings">
                     <div class="cart__item__content__settings__quantity">
-                      <p>Qté : ${article.quantity}</p>
+                      <p>Qté : </p>
                       <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${article.quantity}">
                     </div>
                     <div class="cart__item__content__settings__delete">
@@ -26,52 +25,33 @@ for (article of panier) {
                   </div>
                 </div>
               </article>`
-              // i = String(i);
-            // let evenement = document.querySelector(`#input-${i}`).addEventListener('change', (event) => {
-            //       console.log('ça marche!!')
-            //   })
+  prixFinal += article.price * article.quantity
+
 }
 
+document.querySelector('#totalPrice').innerHTML = prixFinal;
 
+let changeQuantity = document.querySelectorAll('.itemQuantity');
 
+for (el of changeQuantity) {
+  let valeur = el;
+  valeur.addEventListener('change', (e => {
+    let cibleName = e.target.parentElement.parentElement.parentElement.children[0].children[0].innerText;
+    let cibleColor = e.target.parentElement.parentElement.parentElement.children[0].children[1].innerText;
+    panier = panier.map(element => {
+      if (element.name == cibleName && element.color == cibleColor) {
+        let newQuantity =  parseInt(e.target.value);
+        element.quantity = String(newQuantity);
+        bool = true;
+        return element;
+      } else {
+        return element;
+      }
+    })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{/* <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
-                <div class="cart__item__img">
-                  <img src="../images/product01.jpg" alt="Photographie d'un canapé">
-                </div>
-                <div class="cart__item__content">
-                  <div class="cart__item__content__description">
-                    <h2>Nom du produit</h2>
-                    <p>Vert</p>
-                    <p>42,00 €</p>
-                  </div>
-                  <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                      <p>Qté : </p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-                    </div>
-                    <div class="cart__item__content__settings__delete">
-                      <p class="deleteItem">Supprimer</p>
-                    </div>
-                  </div>
-                </div>
-              </article> */}
+    if (bool = true) {
+      localStorage.setItem("product", JSON.stringify(panier))
+      bool = false;
+    }
+  }))
+}
