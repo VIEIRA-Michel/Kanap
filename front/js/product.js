21106e73f6f0544b79c940d401ccc7713bd67863
Michel Vieira <michelvieira.pro@gmail.com>
2022-05-17 18:25:21 +0200

    Refactorisation du code, et ajout de commentaire

--- front/js/product.js
+++ front/js/product.js
@@ -1,81 +1,136 @@
+// On récupère l'url de la page
 let params = new URL(document.location).searchParams;
+// On récupère l'id présent dans l'url et on le stock dans une variable
 let id = params.get("id");
-let modify = false;
-let newArticle = false;
+// On créer un objet vide prêt a recevoir les informations concernant l'article que l'on consulte
+let article = {};
+// article._id = id;
 
-let url = `http://localhost:3000/api/products/${id}`
-
-fetch(url)
+// On execute notre requête en précisant à la fin l'id de larticle pour lequel on souhaite obtenir les informations
+fetch(`http://localhost:3000/api/products/${id}`)
     .then((response) =>
         response.json().then((data) => {
-            const { imageUrl, name, price, description, colors, altTxt } = data;
-            document.querySelector('.item__img').innerHTML =
-                `<img src="${imageUrl}" alt="${altTxt}"></img>`
-            document.querySelector('#title').innerHTML = name;
-            document.querySelector('#price').innerHTML = price;
-            document.querySelector('#description').innerHTML = description;
-            for (let color of colors) {
-                document.querySelector('#colors').innerHTML +=
-                    `<option value="${color}">${color}</option>`
-            }
-            document.querySelector('#addToCart').addEventListener("click", function () {
-                let color = document.querySelector('#colors');
-                let quantity = document.querySelector('#quantity');
-
-                let produit = {
-                    id: id,
-                    image: imageUrl,
-                    alt: altTxt,
-                    description: description,
-                    name: name,
-                    color: color.value,
-                    price: price,
-                    quantity: quantity.value,
-
-                }
-                console.log(produit.color);
-                let produitLocalStorage = JSON.parse(localStorage.getItem("product"));
-
-                function addProductOnLocalStorage() {
-                    produitLocalStorage.push(produit)
-                    localStorage.setItem("product", JSON.stringify(produitLocalStorage))
-                }
-
-
-                if (produit.color == "" || produit.quantity < 1) {
-                    alert("Merci de remplir tous les champs");
-                } else if (produitLocalStorage) {
-                    document.querySelector("#addToCart").style.color = "rgb(0, 205, 0)";
-                    document.querySelector("#addToCart").textContent = "Ajouté à votre panier!";
-                    setTimeout(function () {
-                        document.querySelector("#addToCart").style.color = "rgb(255, 255, 255)";
-                        document.querySelector("#addToCart").textContent = "Ajouter panier!";
-                    }, 500)
-                    produitLocalStorage = produitLocalStorage.map(el => {
-                        if (el.id == produit.id && el.color == produit.color) {
-                            let newNumber = parseInt(el.quantity) + parseInt(produit.quantity);
-                            el.quantity = String(newNumber);
-                            modify = true;
-                            return el;
-                        } else if ((el.color !== produit.color && el.id == produit.id) || (el.id !== produit.id)) {
-                            newArticle = true;
-                            return el;
-                        }
-                    })
-                    if (modify == true) {
-                        localStorage.setItem("product", JSON.stringify(produitLocalStorage));
-                        modify = false;
-                    } else if (newArticle == true) {
-                        addProductOnLocalStorage();
-                        newArticle = false;
-                    }
-
-                } else {
-                    produitLocalStorage = [];
-                    addProductOnLocalStorage();
-                }
-            });
+            displayProduct(data);
         }))
     .catch((error) => {
         console.log(error);
     });
+
+// Fonction qui va nous permettre d'afficher les informations du produit grâce aux données récupérer,
+// et de stocker les données concernant l'article dans un objet
+function displayProduct(data) {
+    const { imageUrl, name, price, description, colors, altTxt } = data;
+    document.querySelector('.item__img').innerHTML =
+        `<img src="${imageUrl}" alt="${altTxt}"></img>`
+    document.querySelector('#title').innerHTML = name;
+    document.querySelector('#price').innerHTML = price;
+    document.querySelector('#description').innerHTML = description;
+    for (let color of colors) {
+        document.querySelector('#colors').innerHTML +=
+            `<option value="${color}">${color}</option>`
+    }
+    article = {
+        id: id,
+        image: imageUrl,
+        alt: altTxt,
+        description: description,
+        name: name,
+        price: price,
+    }
+}
+
+// On met l'écouteur sur le champ concernant la couleur
+// pour le rajouter à notre objet une fois que l'utilisateur aura fait sont choix
+let color = document.querySelector('#colors').addEventListener('change', function (e) {
+    article.color = e.currentTarget.value;
+})
+// On met également l'écouteur sur la quantité
+// pour le rajouter à notre objet une fois que l'utilisateur saura combien il en veux
+let quantity = document.querySelector('#quantity').addEventListener('change', function (e) {
+    article.quantity = e.currentTarget.value;
+})
+
+// Une fois que l'utilisateur clique sur le bouton ajouter au panier, on envoie notre objet "Article"
+// qui a récolter toutes les données jusqu'à présent dans la fonction qui va faire quelque verification
+// avant de l'ajouter au panier
+let addToCart = document.querySelector('#addToCart').addEventListener("click", function () {
+    if (article.color && article.quantity) {
+        addProduct(article);
+    }
+});
+
+function addProduct(article) {
+    let produitLocalStorage = JSON.parse(localStorage.getItem("product"));
+    console.log(article);
+    if (article.color == undefined || article.quantity < 1) {
+        alert("Merci de remplir tous les champs");
+    } else if (produitLocalStorage) {
+        checkProduct(article);
+    } else {
+        produitLocalStorage = [];
+        produitLocalStorage.push(article)
+        addedOnLocalStorage(produitLocalStorage);
+    }
+}
+// On définis une variable que l'on passera a 'true' dans le cas ou l'article que l'on souhaite ajouter au panier 
+// est déjà présent
+let modify = false;
+// On définis une variable que l'on passera a 'true' si l'article n'est pas déjà présent dans le panier 
+// pour l'ajouter comme nouvelle article
+let newArticle = false;
+
+// Cette fonction va nous permettre de faire plusieurs verification afin d'éviter toutes erreurs
+function checkProduct(produit) {
+    // Ici nous récuperons le localStorage que l'on va stocker dans une variable
+    let produitLocalStorage = JSON.parse(localStorage.getItem("product"));
+    // Grâce à la boucle map nous allons pouvoir éplucher chacun des articles présent dans le localstorage
+    produitLocalStorage = produitLocalStorage.map(el => {
+        // Et verifier si l'un deux correspond avec l'article que nous souhaitons ajouter
+        if (el.id == produit.id && el.color == produit.color) {
+            // Si c'est le cas nous stockons l'addition de la quantité de l'article déjà présent dans le panier
+            // et celle que nous nous apprétons à ajouter dans une nouvelle variable
+            let newNumber = parseInt(el.quantity) + parseInt(produit.quantity);
+            // que l'on attribuera ensuite comme nouvelle quantité à l'élement déjà présent dans le panier
+            el.quantity = String(newNumber);
+            // Et nous passons ensuite la variable modify à "true" 
+            modify = true;
+            // Et renvoyer nos modifications dans notre variable produitLocalStorage
+            return el;
+        } else if ((el.color !== produit.color && el.id == produit.id) || (el.id !== produit.id)) {
+            // Si aucun article ne correspond nous passons la variable newArticle à "true"
+            newArticle = true;
+            // et on renvoie également nos modifications dans la variable produitLocalStorage
+            return el;
+        }
+    })
+    animationButton();
+
+    if (modify == true) {
+        // Ici nous allons simplement nous contenter de ré-expéxier de nouveau dans le localStorage
+        addedOnLocalStorage(produitLocalStorage);
+        modify = false;
+    } else if (newArticle == true) {
+        // ici nous allons push notre nouvelle article dans notre variable ou l'on à récuperer
+        // tous les articles déjà présent dans le localStorage
+        produitLocalStorage.push(article)
+        // avant de la ré-expédier de nouveau dans le localStorage
+        addedOnLocalStorage(produitLocalStorage);
+        newArticle = false;
+    }
+
+};
+
+// Animation qui va nous permettre de changer la couleur du bouton une fois qu'un article aura été ajouter a notre panier
+function animationButton() {
+    document.querySelector("#addToCart").textContent = "Ajouté à votre panier!";
+    document.querySelector("#addToCart").style.background = "rgb(0, 205, 0)";
+    setTimeout(function () {
+        document.querySelector("#addToCart").style.background = "rgb(59, 59, 59)";
+        document.querySelector("#addToCart").textContent = "Ajouter panier!";
+    }, 500)
+}
+
+// Fonction qui va ajouter l'élément passer en parametre dans le localStorage
+function addedOnLocalStorage(product) {
+    localStorage.setItem("product", JSON.stringify(product))
+}
